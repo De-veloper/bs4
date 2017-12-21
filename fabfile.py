1bab6f24f9a99b4911b66fa133472b21307a506c
Kevin Chiu <kchiu@paciolan.com>
2017-12-21 13:55:25 -0800

    update: search all or by client

--- fabfile.py
+++ fabfile.py
@@ -14,54 +14,76 @@ def showAllCurClientsFolders():
     print dirs
 
 
-def searchPixelByClient():
-    clientLinkID = raw_input("What client are you looking for (ex:usc) ")
-    filepath = 'C:/gitProjects/clients/%s/pac7.2/script/pixels.js' %clientLinkID
-    textToSearch = raw_input("What pixel are you looking for?(ex:pixel_lib.FB) ")
-    cnt = 0
-    with open(filepath) as fin:
-        for line in fin:
-            if textToSearch in line:
-                #line = 'user_pref("network.proxy.socks", "the.value.i.want");\n'
-                cnt += 1
-    print('%s - %s Pixel fires: %s' %(clientLinkID, textToSearch,cnt) +' times')
-    fin.close()
-@task
-def searchPixelByAllClients():
-    def _createJSON(obj, filename):
-	    with open('%s_pixels.json' %filename, 'w') as outfile:  
-		    json.dump(obj, outfile)
+def searchPixelByClient(clientLinkID):
+    try:
+        filepath = 'C:/gitProjects/clients/%s/pac7.2/script/pixels.js' %clientLinkID
+        if(os.path.exists(filepath)):
+            filepath = 'C:/gitProjects/clients/%s/pac7.2/script/pixels.js' %clientLinkID
+        else:
+            filepath = 'C:/gitProjects/clients/%s/pac7.2/pixel/js/pixels.js' % clientLinkID
     
-    data = {}
-    textToSearch = raw_input("What pixel are you looking for?(ex:pixel_lib.FB) ")
-    root, dirs, files = os.walk('C:/gitProjects/clients/').next()
-    for c in dirs:
-        filepath = 'C:/gitProjects/clients/%s/pac7.2/script/pixels.js' % c
-        #if(os.path.exists(filepath)):
-        try:
-            if(os.path.exists(filepath)):
-                filepath = 'C:/gitProjects/clients/%s/pac7.2/script/pixels.js' % c
-            else:
-                filepath = 'C:/gitProjects/clients/%s/pac7.2/pixel/js/pixels.js' % c
-            
-            cnt = 0
-            with open(filepath) as fin:
-                for line in fin:
-                    if textToSearch in line:
-                        cnt += 1
-            if(cnt > 0):
-                print('%s - %s Pixel fires: %s' %(c, textToSearch,cnt) +' times')
-                data[c] = []
-                data[c].append({
-                    textToSearch:cnt
-                })
-                _createJSON(data,textToSearch)
-                #Todo: create a json file and show a GUI
-                #[
-                #    {'linkID':
-                #        {'FB':3,GA:1}
-                #    }
-                #]
+        pixelName = raw_input("What pixel are you looking for?(ex:pixel_lib.FB) ")
+        cnt = 0
+        with open(filepath) as fin:
+            for line in fin:
+                if pixelName in line:
+                    #line = 'user_pref("network.proxy.socks", "the.value.i.want");\n'
+                    cnt += 1
+        print('%s - %s Pixel fires: %s' %(clientLinkID, pixelName,cnt) +' times')
+        if(cnt>0):
+            data = []
+            data.append({
+                pixelName:cnt
+            })
+            fileName = '%s_%s' %(clientLinkID, pixelName)
+            createJSON(data,fileName)
             fin.close()
-        except:
-            print ('%s - file not found' % c)
+
+    except:
+        print ('%s - file not found' % clientLinkID)
+
+def createJSON(obj, filename):
+    with open('%s_pixels.json' %filename, 'w') as outfile:  
+        json.dump(obj, outfile)
+@task
+def searchPixel():
+    
+    textToSearch = raw_input("Seach all cleints or a spefic client? (ex: leave empty to seach all) ")
+    data = {}
+
+    if (textToSearch !='' ):
+        searchPixelByClient(textToSearch)
+    else:
+        
+        pixelName = raw_input("What pixel are you looking for?(ex:pixel_lib.FB) ")
+        root, dirs, files = os.walk('C:/gitProjects/clients/').next()
+
+        for c in dirs:
+            filepath = 'C:/gitProjects/clients/%s/pac7.2/script/pixels.js' % c
+            try:
+                if(os.path.exists(filepath)):
+                    filepath = 'C:/gitProjects/clients/%s/pac7.2/script/pixels.js' % c
+                else:
+                    filepath = 'C:/gitProjects/clients/%s/pac7.2/pixel/js/pixels.js' % c
+                
+                cnt = 0
+                with open(filepath) as fin:
+                    for line in fin:
+                        if pixelName in line:
+                            cnt += 1
+                if(cnt > 0):
+                    print('%s - %s Pixel fires: %s' %(c, pixelName,cnt) +' times')
+                    data[c] = []
+                    data[c].append({
+                        pixelName:cnt
+                    })
+                    createJSON(data,pixelName)
+                    #Todo: create a json file and show a GUI
+                    #[
+                    #    {'linkID':
+                    #        {'FB':3,GA:1}
+                    #    }
+                    #]
+                fin.close()
+            except:
+                print ('%s - file not found' % c)
